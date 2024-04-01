c8886aeb64ecb2a01fb8a717cf61e85a494db967
blobby-bobby <bulle.ouvrard@gmail.com>
2024-04-01 18:34:28 +0200

    chore: drop ng2-charts, chart.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,16 @@
-import { provideRouter } from "@angular/router";
-import { ApplicationConfig, importProvidersFrom } from "@angular/core";
-import { routes } from "./app-routes";
-import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
-import { BrowserModule } from "@angular/platform-browser";
-import { provideCharts, withDefaultRegisterables } from "ng2-charts";
+import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { routes } from './app-routes';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideRouter(routes),
-        importProvidersFrom(BrowserModule),
-        provideHttpClient(withInterceptorsFromDi()),
-        provideCharts(withDefaultRegisterables()),
-    ],
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(BrowserModule),
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
 };

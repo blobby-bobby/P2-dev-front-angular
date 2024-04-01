14c95d58b6579fa1349d0b6b9291552a56484958
blobby-bobby <bulle.ouvrard@gmail.com>
2024-04-01 18:31:39 +0200

    feat: migration to ngx-charts

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,81 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BadgeType } from 'src/app/core/models/Badge';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { BadgeListComponent } from '../../components/badge-list/badge-list.component';
 import { TitleComponent } from '../../components/title/title.component';
-import { LineChartComponent } from '../../components/line-chart/line-chart.component';
 import { OlympicService } from 'src/app/core/services/olympic.service';
-import { ParticipationType } from 'src/app/core/models/Participation';
 import { OlympicType } from 'src/app/core/models/Olympic';
+import { tap, Subscription, Observable, map } from 'rxjs';
+import { LineGraphData } from 'src/app/core/models/LineGraphData';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { LineChartComponent } from 'src/app/components/line-chart/line-chart.component';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss'],
   standalone: true,
-  imports: [TitleComponent, BadgeListComponent, RouterLink, LineChartComponent],
+  imports: [
+    TitleComponent,
+    BadgeListComponent,
+    RouterLink,
+    NgxChartsModule,
+    LineChartComponent,
+  ],
 })
-export class DetailComponent implements OnInit {
-  datas: BadgeType[] = [];
-  pageTitle: string = '';
-
-  lineChartLabels: string[] = [];
-  numberOfMedalsByYear: number[] = [];
+export class DetailComponent implements OnInit, OnDestroy {
+  public datas: BadgeType[] = [];
+  public pageTitle!: string;
+  private subscription!: Subscription;
+  public selectedCountryId!: number;
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private olympicService: OlympicService
-  ) {}
+  ) {
+    this.selectedCountryId = 0;
+  }
 
-  /**
-   * Initialize the component and retrieve country data based on the route parameter.
-   *
-   * @return {void}
-   */
   ngOnInit(): void {
-    const countryId = Number(this.route.snapshot.paramMap.get('id'));
-    if (countryId) {
-      this.olympicService
-        .getCountryById(countryId)
-        .subscribe((countryData: OlympicType | undefined) => {
-          // Update the datas badgesand page title with the retrieved country data
-          this.pageTitle = countryData?.country || 'Country';
+    this.selectedCountryId = Number(this.route.snapshot.params['id']);
+    this.subscription = this.olympicService
+      .getCountryById(this.selectedCountryId)
+      .pipe(
+        tap((olympic: OlympicType) => {
+          if (!olympic) {
+            this.router.navigateByUrl('pageNotFound');
+          }
+
+          this.pageTitle = olympic.country;
+
           this.datas = [
             {
               datakey: 'Number of entries',
-              datavalue: countryData?.participations.length,
+              datavalue: olympic.participations.length,
             },
             {
               datakey: 'Total number medals',
-              datavalue: countryData?.participations.reduce(
-                (total: number, participation: ParticipationType) =>
-                  total + participation.medalsCount,
+              datavalue: olympic.participations.reduce(
+                (acc, curr) => acc + curr.medalsCount,
                 0
               ),
             },
             {
               datakey: 'Total number of athletes',
-              datavalue: countryData?.participations.reduce(
-                (total: number, participation: ParticipationType) =>
-                  total + participation.athleteCount,
+              datavalue: olympic.participations.reduce(
+                (acc, curr) => acc + curr.athleteCount,
                 0
               ),
             },
           ];
+        })
+      )
+      .subscribe();
+  }
 
-          /**
-           * Update the line chart labels and data based on the retrieved country data
-           */
-          this.lineChartLabels =
-            countryData?.participations.map(
-              (participation: ParticipationType) =>
-                participation.year.toString()
-            ) || [];
-          this.numberOfMedalsByYear =
-            countryData?.participations.map(
-              (participation: ParticipationType) => participation.medalsCount
-            ) || [];
-        });
-    }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
